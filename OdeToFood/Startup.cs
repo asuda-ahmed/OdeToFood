7908fe94a722525d77af0c66ea5280117656b662
Asuda Ahmed <asuda.ahmed@workingtime-solutions.com>
2020-01-23 15:23:29 +0000

    some new features added

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -31,7 +31,7 @@ namespace OdeToFood
             });
 
             services.AddScoped<IRestaurantData, PostgreSQLRestaurantData>();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
